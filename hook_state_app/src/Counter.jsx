a971d8b1f37297b0ab65390b760a419b230d63ad
Shyamal Sardar <88072892+Shyamal90@users.noreply.github.com>
2022-02-01 09:05:09 +0530

    complete assignment

--- hook_state_app/src/Counter.jsx
+++ hook_state_app/src/Counter.jsx
@@ -4,15 +4,27 @@ import './counter.css';
 function Counter({initial_value}) {
   
     //state of the counter
-    const [currValue,setCurrValue] = useState(initial_value);
+    let [currValue,setCurrValue] = useState(initial_value);
 
+
+    //Change state 
+    const changeValue = (query) =>{
+        if(query === 1 || query === -1){
+            currValue += query;
+        }else if(query === 2){
+            currValue *= query;
+        }
+
+        //change counter value
+        setCurrValue(currValue);
+    }
   return (
     <div id='container'>
        <p id='counter'>{currValue}</p>
        <div className="operation">
-           <button id='increment'>Increment</button>
-           <button id='decrement'>Decrement</button>
-           <button id='double'>Double</button>
+           <button id='increment' onClick={()=> changeValue(1)}>Increment</button>
+           <button id='decrement' onClick={()=> changeValue(-1)}>Decrement</button>
+           <button id='double' onClick={()=> changeValue(2)}>Double</button>
        </div>
     </div>
   )
